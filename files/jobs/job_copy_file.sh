a9a4209163ff72902f1aa2e6d9792fe87e257dd6
Deivid Duarte <48442376+deividduarte20@users.noreply.github.com>
2025-07-13 14:57:36 -0300

    Update job_copy_file.sh

--- files/jobs/job_copy_file.sh
+++ files/jobs/job_copy_file.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-# Variaveis
-PASS=toor
-USER=root
-DIR_DEST=${1}
-FILE=${2}
-FILE_NAME=${3}
-IP=${4}
+# Variáveis
+DIR_DEST="${1}"
+FILE="${2}"
+FILE_NAME="${3}"
+IP="${4}"
+USER="${5}"
+PASS="${6}"
 
 # Renomeia arquivo para nome e extensão de origem
 mv "$FILE" "$FILE_NAME"
 
 # Transfere arquivo através do protocolo ssh
-sshpass -p "$PASS" scp -v -o StrictHostKeyChecking=no "$FILE_NAME" "$USER@$IP:$DIR_DEST"
+if sshpass -p "$PASS" scp -o StrictHostKeyChecking=no "$FILE_NAME" "$USER@$IP:$DIR_DEST"; then
+  echo "Arquivo copiado com sucesso: $FILE_NAME para $DIR_DEST em $IP"
+  rm -rf $FILE_NAME
+else
+  echo "Erro ao copiar o arquivo: $FILE_NAME"
+  exit 1
+fi
